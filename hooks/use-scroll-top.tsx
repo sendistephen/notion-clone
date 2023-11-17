75dc8c0dae3656fd73c15f1b63a322258733914b
Ssendikadiwa Stephen <sendi.stev@gmail.com>
2023-11-17 11:46:11 +0300

    Add marketing welcome page

---
+++ hooks/use-scroll-top.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+
+const useScrollTop = (threshold = 10) => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > threshold) {
+        setScrolled(true); // user has scrolled
+      } else {
+        setScrolled(false);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [threshold]);
+
+  return scrolled;
+};
+
+export default useScrollTop;
