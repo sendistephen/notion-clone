368da900aa1dc2de7b018d1b1bf80b69cb355cc2
sendistephen <sendi.stev@gmail.com>
2023-11-21 10:42:16 +0300

    Add redirects to document page

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -2,9 +2,15 @@
 
 import { Button } from '@/components/ui/button';
 import { ArrowRightIcon } from '@radix-ui/react-icons';
+import { useConvexAuth } from 'convex/react';
 import React from 'react';
+import { Spinner } from './spinner';
+import Link from 'next/link';
+import { SignInButton } from '@clerk/clerk-react';
+import { ArrowRight } from 'lucide-react';
 
 const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   return (
     <div className='max-w-3xl space-y-4'>
       <h1 className='text-3xl sm:text-5xl md:text-6xl font-bold'>
@@ -14,9 +20,25 @@ const Heading = () => {
       <h3 className='text-base sm:text-xl md:text-2xl font-medium'>
         Jotion is the connected workspace where better, faster work happens.
       </h3>
-      <Button>
-        Enter Jotion <ArrowRightIcon className='h-4 w-4 ml-2' />
-      </Button>
+      {isLoading && (
+        <div className='w-full grid place-items-center'>
+          <Spinner size='lg' />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button>
+          <Link href='/documents'>
+            Enter Jotion <ArrowRightIcon className='h-4 w-4 ml-2' />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode='modal'>
+          <Button>
+            <span>Get Jotion free</span> <ArrowRight className='h-4 w-4 ml-2' />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   );
 };
